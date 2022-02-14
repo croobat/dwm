8aee1ee4e64bc86c3387acb8365db5d9e4c07e6a
Tony Ramírez <tonyram9906@outlook.com>
2022-02-13 21:58:47 -0600

    warp patch, systray patch

--- config.h
+++ config.h
@@ -4,10 +4,16 @@
 
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 4;        /* gaps between windows */
-static const unsigned int snap      = 24;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
+static const int showbar            = 1;     /* 0 means no bar */
+static const int topbar             = 1;     /* 0 means bottom bar */
+static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const char *fonts[]          = { "JetBrains Mono Nerd Font Medium:size=10" };
 static const char dmenufont[]       = "JetBrains Mono Nerd Font Medium:size=10";
 static const char col_black[]               = "#282a36";
@@ -35,15 +41,15 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
-	{ "Godot",       NULL,                  NULL,         1 << 2,       0,            0 },
-    { "VSCodium",    "vscodium",            NULL,         1 << 1,       0,            0 },
-    { NULL,          NULL,                  "WhatsApp",   1 << 4,       0,            1 },
-    { "discord",     NULL,                  NULL,         1 << 4,       0,            1 },
+	/* class     instance           title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",        NULL,          NULL,           0,         1,          0,           0,        -1 },
+	{ "Firefox",     NULL,          NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "St",          NULL,          NULL,           0,         0,          1,           0,        -1 },
+	{ NULL,          NULL,          "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	{ "Godot",       NULL,          NULL,           1 << 2,    0,          0,           0,         0 },
+    { "VSCodium",    "vscodium",    NULL,           1 << 1,    0,          0,           0,         0 },
+    { NULL,          NULL,          "WhatsApp",     1 << 4,    0,          0,           0,         1 },
+    { "discord",     NULL,          NULL,           1 << 4,    0,          0,           0,         1 },
 };
 
 /* layout(s) */
@@ -127,8 +133,8 @@ static Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
