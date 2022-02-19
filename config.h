f247c7241dda1223115fec0d6baea670f7a7eb64
Tony Ramírez <tonyram9906@outlook.com>
2022-02-19 17:45:18 -0600

    More rules added

--- config.h
+++ config.h
@@ -49,6 +49,8 @@ static const Rule rules[] = {
 	{ "Godot",       NULL,          NULL,           1 << 2,    0,          0,           0,         0 },
 	{ "qutebrowser", NULL,          NULL,           1 << 0,    0,          0,           0,        -1 },
     { "VSCodium",    "vscodium",    NULL,           1 << 1,    0,          0,           0,         0 },
+    { NULL,          NULL,          "ncspot",       1 << 3,    0,          0,           0,         1 },
+    { NULL,          NULL,          "cmus v2.9.1",  1 << 3,    0,          0,           0,         1 },
     { NULL,          NULL,          "WhatsApp",     1 << 4,    0,          0,           0,         1 },
     { "discord",     NULL,          NULL,           1 << 4,    0,          0,           0,         1 },
 };
