80f6097832ccd6cc1f673ffcda27d81aa7217021
Tony Ramirez <tonyramirez.business@outlook.com>
2022-08-26 12:49:30 -0500

    added tmux to scratchpad

--- config.h
+++ config.h
@@ -134,7 +134,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_white, "-sb", col_light_gray, "-sf", col_white, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "alacritty", "-t", scratchpadname, "--config-file", "/home/tony/.config/dwm/patches/scratchpad.yml", NULL };
+static const char *scratchpadcmd[] = { "alacritty", "-t", scratchpadname, "--config-file", "/home/tony/.config/dwm/patches/scratchpad.yml", "-e", "tmux", NULL };
 
 /* Patches */
 #include "patches/shiftview.c" // Tag rotation
