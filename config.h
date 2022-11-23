a4411644c6c98405b72073fce55d29d8ccbe1876
Tony Ramírez <tonyramirez.business@outlook.com>
2022-11-22 22:21:32 -0600

    first

--- config.h
+++ config.h
@@ -2,31 +2,33 @@
 
 /* appearance */
 
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 2;        /* gaps between windows */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
-static const int showbar            = 1;     /* 0 means no bar */
-static const int topbar             = 1;     /* 0 means bottom bar */
-static const Bool viewontag         = True;     /* Switch view on tag switch */
+static const unsigned int borderpx        = 2;    /* border pixel of windows */
+static const unsigned int gappx           = 0;    /* gaps between windows */
+static const int swallowfloating          = 0;    /* 1 means swallow floating windows by default */
+static const unsigned int snap           = 32;    /* snap pixel */
+static const unsigned int systraypinning  = 0;    /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft   = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing  = 2;    /* systray spacing */
+static const int systraypinningfailfirst  = 1;    /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray              = 1;    /* 0 means no systray */
+static const int showbar                  = 1;    /* 0 means no bar */
+static const int topbar                   = 1;    /* 0 means bottom bar */
+static const Bool viewontag            = True;    /* Switch view on tag switch */
+
 static const char *fonts[]          = { "JetBrains Mono Nerd Font:size=9:style=Bold" };
 static const char dmenufont[]       = { "JetBrains Mono Nerd Font:size=9:style=Bold" };
-static const char col_black[]               = "#282a36";
-static const char col_white[]               = "#f8f8f2";
-static const char col_light_gray[]          = "#44475a";
-static const char col_blue[]                = "#6272a4";
-static const char col_cyan[]                = "#8be9fd";
-static const char col_green[]               = "#50fa7b";
-static const char col_orange[]              = "#ffb86c";
-static const char col_pink[]                = "#ff79c6";
-static const char col_purple[]              = "#bd93f9";
-static const char col_red[]                 = "#ff5555";
-static const char col_yellow[]              = "#f1fa8c";
+static const char col_black[]       = "#282a36";
+static const char col_white[]       = "#f8f8f2";
+static const char col_light_gray[]  = "#44475a";
+static const char col_blue[]        = "#6272a4";
+static const char col_cyan[]        = "#8be9fd";
+static const char col_green[]       = "#50fa7b";
+static const char col_orange[]      = "#ffb86c";
+static const char col_pink[]        = "#ff79c6";
+static const char col_purple[]      = "#bd93f9";
+static const char col_red[]         = "#ff5555";
+static const char col_yellow[]      = "#f1fa8c";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_white, col_black, col_black },
@@ -55,69 +57,65 @@ static const char sscreenrec[] = "simplescreenrecorder";
 static const char steam_payp[] = "Log in to your PayPal account";
 static const char firefox_dev[] = "firefoxdeveloperedition";
 static const char chrome_dev[] = "Google-chrome-unstable";
+static const char foliate[] = "com.github.johnfactotum.Foliate";
 
 static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
-	/* class     instance           title         tags mask  isfloating  isterminal  noswallow  monitor */
-	// Non floating windows
-	// Monitor -1
-	{ "qutebrowser", NULL,          NULL,           web,     0,          0,           0,        -1 },
-	{ firefox_dev,   NULL,          NULL,           brow,    0,          0,           0,        -1 },
-	{ chrome_dev,    NULL,          NULL,           brow,    0,          0,           0,        -1 },
-	{ NULL,          "libreoffice", NULL,           doc,     0,          0,           0,        -1 },
-	{ NULL,          NULL,          "LibreOffice",  doc,     0,          0,           0,        -1 },
-	{ NULL,          win_x11_xframe,NULL,           doc,     0,          0,           0,        -1 },
-	{ "calibre",     NULL,          NULL,           doc,     0,          0,           0,        -1 },
-	{ "qBittorrent", NULL,          NULL,           back,    0,          0,           0,        -1 },
-	// Monitor 0
-	{ "VSCodium",    "vscodium",    NULL,           doc,     0,          0,           0,         0 },
-	{ "Godot",       NULL,          NULL,           doc,     0,          0,           0,         0 },
-	{ "Steam",       NULL,          NULL,           fun,     0,          0,           0,         0 },
-	// Monitor 1
-	{ NULL,          NULL,          "sncli",        doc,     0,          0,           0,         1 },
-	{ "thunderbird", NULL,          NULL,           doc,     0,          0,           0,         1 },
-	{ NULL,          NULL,          "cmus",         play,    0,          0,           0,         1 },
-	{ NULL,          NULL,          "ncspot",       play,    0,          0,           0,         1 },
-	{ NULL,          NULL,          "rofi-beats",   play,    0,          0,           0,         1 },
-	{ NULL,          NULL,          "WhatsApp",     chat,    0,          0,           0,         1 },
-	{ "Slack",       NULL,          NULL,           chat,    0,          0,           0,         1 },
-	{ "discord",     NULL,          NULL,           call,    0,          0,           0,         1 },
-	{ "zoom",        NULL,          NULL,           call,    0,          0,           0,         1 },
-	{ "Trello",      NULL,          NULL,           fun,     0,          0,           0,         1 },
-	{ NULL,          sscreenrec,    NULL,           back,    0,          0,           0,         1 },
-	{ NULL,          NULL,          "*DEBUG*",      doc,     0,          0,           0,         1 },
+    /* xprop(1):
+     *	WM_CLASS(STRING) = instance, class
+     *	WM_NAME(STRING) = title
+     */
+    /* class        instance        title           mask   float term  swallow monitor */
+    // Non floating windows
+    { "qutebrowser", NULL,          NULL,           web,     0,    0,    0,    -1 },
+    { firefox_dev,   NULL,          NULL,           brow,    0,    0,    0,    -1 },
+    { chrome_dev,    NULL,          NULL,           brow,    0,    0,    0,    -1 },
+    { NULL,          "libreoffice", NULL,           doc,     0,    0,    0,    -1 },
+    { NULL,          NULL,          "LibreOffice",  doc,     0,    0,    0,    -1 },
+    { NULL,          win_x11_xframe,NULL,           doc,     0,    0,    0,    -1 },
+    { "calibre",     NULL,          NULL,           doc,     0,    0,    0,    -1 },
+    { NULL,          NULL,          "*DEBUG*",      doc,     0,    0,    0,    -1 },
+    { "VSCodium",    "vscodium",    NULL,           doc,     0,    0,    0,    -1 },
+    { "Godot",       NULL,          NULL,           doc,     0,    0,    0,    -1 },
+    { NULL,          NULL,          "sncli",        doc,     0,    0,    0,    -1 },
+    { NULL,          NULL,          "Google Docs",  doc,     0,    0,    0,    -1 },
+    { "thunderbird", NULL,          NULL,           doc,     0,    0,    0,    -1 },
+    { NULL,          foliate,       NULL,           app,     0,    0,    0,    -1 },
+    { NULL,          NULL,          "cmus",         play,    0,    0,    0,    -1 },
+    { NULL,          NULL,          "ncspot",       play,    0,    0,    0,    -1 },
+    { NULL,          NULL,          "rofi-beats",   play,    0,    0,    0,    -1 },
+    { NULL,          NULL,          "WhatsApp",     chat,    0,    0,    0,    -1 },
+    { "Slack",       NULL,          NULL,           chat,    0,    0,    0,    -1 },
+    { "discord",     NULL,          NULL,           call,    0,    0,    0,    -1 },
+    { "zoom",        NULL,          NULL,           call,    0,    0,    0,    -1 },
+    { "Trello",      NULL,          NULL,           fun,     0,    0,    0,    -1 },
+    { "Steam",       NULL,          NULL,           fun,     0,    0,    0,    -1 },
+    { NULL,          "retroarch",   NULL,           fun,     0,    0,    0,    -1 },
+    { NULL,          sscreenrec,    NULL,           back,    0,    0,    0,    -1 },
+    { "qBittorrent", NULL,          NULL,           back,    0,    0,    0,    -1 },
 
-	// Floating windows
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
-	/* class     instance           title           tags mask  isfloating  isterminal  noswallow  monitor */
-	// Monitor -1
-	{ "Gimp",        NULL,          NULL,           doc,       1,          0,           0,        -1 },
-	{ "Pavucontrol", NULL,          NULL,           0,         1,          0,           0,        -1 },
-	{ "Gcolor3",     NULL,          NULL,           0,         1,          0,           0,        -1 },
-	{ NULL,          NULL,          "Pick",         0,         1,          0,           0,        -1 },
-	{ NULL,          NULL,          win_qemu,       0,         1,          0,           0,        -1 },
-	{ "fontforge",   NULL,          "Open Font",    0,         1,          0,           0,        -1 },
-	{ "fontforge",   NULL,          "Warnings",     0,         1,          0,           0,        -1 },
-	{ "Steam",       NULL,          "Friends List", 0,         1,          0,           0,        -1 },
-	{ NULL,          NULL,          win_xampp,      0,         1,          0,           0,        -1 },
-	{ "Gitk",        NULL,          NULL,           0,         1,          0,           0,        -1 },
-	{ NULL,          "peek",        NULL,           0,         1,          0,           0,        -1 },
-	{ NULL,          NULL,          steam_payp,     0,         1,          0,           0,        -1 },
-	// Monitor 0
-
-	// Swallow windows
-	{ "Alacritty",   NULL,          NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,          NULL,          "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+    /* class        instance        title           mask   float term  swallow monitor */
+    { "Gimp",        NULL,          NULL,           doc,     1,    0,    0,    -1 },
+    { "Pavucontrol", NULL,          NULL,           0,       1,    0,    0,    -1 },
+    { "Gcolor3",     NULL,          NULL,           0,       1,    0,    0,    -1 },
+    { NULL,          NULL,          "Pick",         0,       1,    0,    0,    -1 },
+    { NULL,          NULL,          win_qemu,       0,       1,    0,    0,    -1 },
+    { "fontforge",   NULL,          "Open Font",    0,       1,    0,    0,    -1 },
+    { "fontforge",   NULL,          "Warnings",     0,       1,    0,    0,    -1 },
+    { "Steam",       NULL,          "Friends List", 0,       1,    0,    0,    -1 },
+    { NULL,          NULL,          win_xampp,      0,       1,    0,    0,    -1 },
+    { "Gitk",        NULL,          NULL,           0,       1,    0,    0,    -1 },
+    { NULL,          "peek",        NULL,           0,       1,    0,    0,    -1 },
+    { NULL,          NULL,          steam_payp,     0,       1,    0,    0,    -1 },
+    { NULL,          NULL,          "Bluetooth",    0,       1,    0,    0,    -1 },
+    	// Swallow windows
+    /* class        instance        title           mask   float term  swallow monitor */
+    { "Alacritty",   NULL,          NULL,           0,       0,    1,    0,    -1 },
+    { NULL,          "sddm_greeter",NULL,           0,       0,    0,    1,    -1 },
+    { NULL,          NULL,          "Event Tester", 0,       0,    0,    1,    -1 }, /* xev */
 };
 
 /* layout(s) */
-static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
@@ -159,17 +157,18 @@ static const char *scratchpadcmd[] = { "alacritty",
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,	                      XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_space,  togglescratch,  {.v = scratchpadcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,	                    XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,	                    XK_e,      spawn,          {.v = termcmd } },
+	{ MODKEY,                    XK_backslash, togglescratch,  {.v = scratchpadcmd } },
+	{ MODKEY,                       XK_x,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_Tab,    focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_z,      zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    focusmon,       {.i = +1 } },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[1]} },
@@ -186,7 +185,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 	{ MODKEY|ShiftMask,             XK_k,      pushup,         {0} },
-  { MODKEY|ShiftMask,             XK_j,      pushdown,       {0} },
+	{ MODKEY|ShiftMask,             XK_j,      pushdown,       {0} },
 	{ MODKEY|ControlMask,           XK_comma,  tagswapmon,     {.i = +1 } },
 	{ MODKEY|ControlMask,           XK_period, tagswapmon,     {.i = -1 } },
 	TAGKEYS(                        XK_1,                      0)
@@ -218,4 +217,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
